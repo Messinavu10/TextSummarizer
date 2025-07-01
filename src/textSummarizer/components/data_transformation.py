f079edc765dd9d48bf6ecb236acd7bb46540599b
Messinavu10 <messinavu10@gmail.com>
2025-06-30 18:54:22 -0700

    added model trainer

--- src/textSummarizer/components/data_transformation.py
+++ src/textSummarizer/components/data_transformation.py
@@ -10,18 +10,34 @@ class DataTransformation:
     def __init__(self,config:DataTransformationConfig):
         self.config=config
         self.tokenizer=AutoTokenizer.from_pretrained(config.tokenizer_name)
-
+    
     def convert_examples_to_features(self,example_batch):
         input_encodings = self.tokenizer(example_batch['dialogue'] , max_length = 1024, truncation = True )
 
-        with self.tokenizer.as_target_tokenizer():
-            target_encodings = self.tokenizer(example_batch['summary'], max_length = 128, truncation = True )
+        # For Falconsai model, use text_target instead of as_target_tokenizer
+        target_encodings = self.tokenizer(
+            text_target=example_batch['summary'], 
+            max_length = 128, 
+            truncation = True 
+        )
 
         return {
             'input_ids' : input_encodings['input_ids'],
             'attention_mask': input_encodings['attention_mask'],
             'labels': target_encodings['input_ids']
         }
+
+    # def convert_examples_to_features(self,example_batch):
+    #     input_encodings = self.tokenizer(example_batch['dialogue'] , max_length = 1024, truncation = True )
+
+    #     with self.tokenizer.as_target_tokenizer():
+    #         target_encodings = self.tokenizer(example_batch['summary'], max_length = 128, truncation = True )
+
+    #     return {
+    #         'input_ids' : input_encodings['input_ids'],
+    #         'attention_mask': input_encodings['attention_mask'],
+    #         'labels': target_encodings['input_ids']
+    #     }
     
     def convert(self):
         dataset_samsum = load_from_disk(self.config.data_path)
